afa65be239064df8f60a63f59a9ca825c1f78d52
jlg777 <proyectojlg777@gmail.com>
2023-11-28 21:22:17 -0300

    frontend-feature: se completa html y css del login

--- travels-app-frontend/src/routes/PublicRoutes.jsx
+++ travels-app-frontend/src/routes/PublicRoutes.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import { Outlet } from "react-router-dom";
 import HomePage from '../pages/homePage';
 import RegisterPage from '../pages/RegisterPage';
+import LoginPage from '../pages/LoginPage';
 
 function AppPublickRouter() {
     return (
       <Routes>
         <Route path="/" element={<HomePage />}  />
         <Route path="/register" element={<RegisterPage />}  />
+        <Route path="/login" element={<LoginPage />}  />
   
         <Route path="*" element={<Outlet />}/>
       </Routes>
