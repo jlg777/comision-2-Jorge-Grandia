d464ce081ed0c37c1160b1fa3ea6bea85c6cdd9e
= <=>
2023-11-29 10:28:02 -0300

    frontend-feature/login: Se completo rutas de logueo y logica

--- travels-app-frontend/src/routes/PrivateRoutes.jsx
+++ travels-app-frontend/src/routes/PrivateRoutes.jsx
@@ -1,12 +1,12 @@
-import { Route, Routes } from 'react-router-dom';
-import IndexPage from '../pages/IndexPage';
-import PrivateRoutes from '../components/PrivateRoutes';
+import { Route, Routes } from "react-router-dom";
+import IndexPage from "../pages/IndexPage";
+import PrivateRoutes from "../components/PrivateRoutes";
 
 function AppPrivateRouter() {
-    return (
-      <Routes element={<PrivateRoutes />}>
-        <Route path="/index" element={<IndexPage />} />
-      </Routes>
-    )
-  }
-  export default AppPrivateRouter;
+  return (
+    <Route element={<PrivateRoutes />}>
+      <Route path="/app/index" element={<IndexPage />} />
+    </Route>
+  );
+}
+export default AppPrivateRouter;
--- travels-app-frontend/src/routes/PublicRoutes.jsx
+++ travels-app-frontend/src/routes/PublicRoutes.jsx
@@ -1,18 +1,24 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import { Outlet } from "react-router-dom";
-import HomePage from '../pages/homePage';
-import RegisterPage from '../pages/RegisterPage';
-import LoginPage from '../pages/LoginPage';
+import HomePage from "../pages/homePage";
+import RegisterPage from "../pages/RegisterPage";
+import LoginPage from "../pages/LoginPage";
+import PrivateRoutes from "../components/PrivateRoutes";
+import IndexPage from "../pages/IndexPage";
 
 function AppPublickRouter() {
-    return (
-      <Routes>
-        <Route path="/" element={<HomePage />}  />
-        <Route path="/register" element={<RegisterPage />}  />
-        <Route path="/login" element={<LoginPage />}  />
-  
-        <Route path="*" element={<Outlet />}/>
-      </Routes>
-    );
-  }
-  export default AppPublickRouter;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
+
+      <Route path="*" element={<Outlet />} />
+
+      <Route element={<PrivateRoutes />}>
+        <Route path="/app/index" element={<IndexPage />} />
+      </Route>
+    </Routes>
+  );
+}
+export default AppPublickRouter;
