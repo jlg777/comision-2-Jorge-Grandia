a7f0518971ce914e4572a864d4ce6e5ae25a46b0
jlg777 <proyectojlg777@gmail.com>
2023-12-04 22:02:37 -0300

    frontend-change: Se modificaron los botones del navbar con las 3 rutas existentes

--- travels-app-frontend/src/components/NavBar.jsx
+++ travels-app-frontend/src/components/NavBar.jsx
@@ -5,13 +5,11 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import Button from "react-bootstrap/Button";
 import { BsPersonFill } from "react-icons/bs";
 
-
-
 export const NavbarApp = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand" href="/">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             xmlnsXlink="http://www.w3.org/1999/xlink"
@@ -29,24 +27,15 @@ export const NavbarApp = () => {
             ></path>
           </svg>
         </a>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="login">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Button variant="outline-secondary"><BsPersonFill /></Button>{" "}
+            <Nav.Link href="/register">Registro</Nav.Link>
+            <Nav.Link href="posts">Posts</Nav.Link>
+            <Button href="login" variant="outline-secondary" >
+              <BsPersonFill />
+            </Button>{" "}
           </Nav>
         </Navbar.Collapse>
       </Container>
