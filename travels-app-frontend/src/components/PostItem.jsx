86b12775a694aa56060182010e0ecf5f1cfb0083
jlg777 <proyectojlg777@gmail.com>
2023-12-04 21:31:29 -0300

    backend-feature/post: Se completo mapeado de card

--- travels-app-frontend/src/components/PostItem.jsx
+++ travels-app-frontend/src/components/PostItem.jsx
@@ -6,36 +6,46 @@ import { BsFillTrash3Fill } from "react-icons/bs";
 import { BsEyeFill } from "react-icons/bs";
 
 const PostItem = ({
-  title,
+  posts
+  /*title,
   description,
   username,
   comments,
   image,
-  createdAt,
+  createdAt,*/
 }) => {
-  return (          
-    <div className="container-fluid d-flex  flex-colum justify-content-center align-items-center">
-      <Card style={{ width: "25rem" }}>
-        <Card.Img variant="top" src={image} />
-        <Card.Body>
-          <Card.Title>{title}</Card.Title>
-          <Card.Text>{description}</Card.Text>
-        </Card.Body>
-        <ListGroup className="list-group-flush">
-          <ListGroup.Item>{username}</ListGroup.Item>
-          <ListGroup.Item>{comments}</ListGroup.Item>
-          <ListGroup.Item>{createdAt}</ListGroup.Item>
-        </ListGroup>
-        <Card.Body>
-          <Button variant="outline-success">
-            <BsEyeFill />
-          </Button>{" "}
-          <Button variant="outline-danger">
-            <BsFillTrash3Fill />
-          </Button>{" "}
-        </Card.Body>
-      </Card>
-    </div>
+  return ( 
+    <>   
+    {posts.map(
+      (post)=>(
+        <div className="container-fluid d-flex  flex-colum justify-content-center align-items-center"
+        key={post._id}
+        >
+        <Card style={{ width: "25rem" }}>
+          <Card.Img variant="top" src={post.image} />
+          <Card.Body>
+            <Card.Title>{post.title}</Card.Title>
+            <Card.Text>{post.description}</Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>{post.username}</ListGroup.Item>
+            <ListGroup.Item>{post.comments}</ListGroup.Item>
+            <ListGroup.Item>{post.createdAt}</ListGroup.Item>
+          </ListGroup>
+          <Card.Body>
+            <Button variant="outline-success">
+              <BsEyeFill />
+            </Button>{" "}
+            <Button variant="outline-danger">
+              <BsFillTrash3Fill />
+            </Button>{" "}
+          </Card.Body>
+        </Card>
+      </div>
+      )
+    )}      
+    
+    </>
   );
 };
 
