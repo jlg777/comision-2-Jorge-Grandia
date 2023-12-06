61821065caf8aef857bf596723a00a230a5d4511
jlg777 <proyectojlg777@gmail.com>
2023-12-06 01:02:35 -0300

    frontedn-feature/delete/posts: update error

--- travels-app-frontend/src/components/PostItem.jsx
+++ travels-app-frontend/src/components/PostItem.jsx
@@ -5,46 +5,51 @@ import Button from "react-bootstrap/Button";
 import { BsFillTrash3Fill } from "react-icons/bs";
 import { BsEyeFill } from "react-icons/bs";
 
-const PostItem = ({
-  posts
-  /*title,
-  description,
-  username,
-  comments,
-  image,
-  createdAt,*/
-}) => {
-  return ( 
-    <>   
-    {posts.map(
-      (post)=>(
-        <div className="container-fluid d-flex  flex-colum justify-content-center align-items-center"
-        key={post._id}
+const PostItem = ({posts}) => {
+  const handleDelete = async (postsID) => {
+   return await fetch(`http://localhost:3000/post/${postsID}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+  return (
+    <>
+      {posts.map((post) => (
+        <div
+          className="container-fluid d-flex  flex-colum justify-content-center align-items-center"
+          key={post._id}
         >
-        <Card style={{ width: "25rem" }}>
-          <Card.Img variant="top" src={post.image} />
-          <Card.Body>
-            <Card.Title>{post.title}</Card.Title>
-            <Card.Text>{post.description}</Card.Text>
-          </Card.Body>
-          <ListGroup className="list-group-flush">
-            <ListGroup.Item>{post.username}</ListGroup.Item>
-            <ListGroup.Item>{post.comments}</ListGroup.Item>
-            <ListGroup.Item>{post.createdAt}</ListGroup.Item>
-          </ListGroup>
-          <Card.Body>
-            <Button variant="outline-success">
-              <BsEyeFill />
-            </Button>{" "}
-            <Button variant="outline-danger">
-              <BsFillTrash3Fill />
-            </Button>{" "}
-          </Card.Body>
-        </Card>
-      </div>
-      )
-    )}      
-    
+          <Card style={{ width: "25rem" }}>
+            <Card.Img variant="top" src={post.image} />
+            <Card.Body>
+              <Card.Title>{post.title}</Card.Title>
+              <Card.Text>{post.description}</Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>{post.username}</ListGroup.Item>
+              <ListGroup.Item>{post.comments}</ListGroup.Item>
+              <ListGroup.Item>{post.createdAt}</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Button variant="outline-success">
+                <BsEyeFill />
+              </Button>{" "}
+              <Button variant="outline-danger">
+                <BsFillTrash3Fill 
+                onClick = {() => 
+                  handleDelete()
+                  .then(res=>{
+                    if (res.status != 200)
+                    alert('No se elimina')
+                  else  alert('Se elimina')
+                  })}/>
+              </Button>{" "}
+            </Card.Body>
+          </Card>
+        </div>
+      ))}
     </>
   );
 };
